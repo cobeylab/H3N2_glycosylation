d6fc27ce577f1f5ca771143d251386cdfc33df57
Sarah Cobey <cobey@uchicago.edu>
2017-11-06 16:22:25 -0600

    Updated code for revised ms

--- Serology/Serological_analyses.R
+++ Serology/Serological_analyses.R
@@ -9,8 +9,7 @@
 #
 # SECTIONS
 # - Data import & preparation (must execute)
-# - (Figures) Pre- & post-vaccination trends
-# - (Statistics) Correlations by age
+# - (Statistics) Correlations by age, 160K/T
 # - (Figures) Fold change by vaccine group
 # - (Figures) Vaccination history & boost
 #
@@ -33,10 +32,6 @@ require(ggplot2)
 
 # Import FRNT data (with two columns of Vx history)
 titers_raw <- read.table("2017-04-19_FRNT_for_import.csv",header=TRUE,sep=",",na.strings="Unknown")
-H3_exp <- read.table("H3_exp.csv",sep=",")
-colnames(H3_exp) <- c("YOB","pH3")
-T160K_exp <- read.table("160K_exp.csv",sep=",",header=FALSE)
-colnames(T160K_exp) <- c("YOB","p160K")
 
 # Merge data and add basic statistics
 titers <- titers_raw
@@ -61,8 +56,6 @@ titers$Pre_ratio_WT_160K <- titers$GMT_Pre_WT/titers$GMT_Pre_160K
 # Define ordinal variables based on vaccination history
 # and add probability of primary exposure to H3
 titers$Vx_hx <- NA
-titers$H3_exp <- NA
-titers$T160K_exp <- NA
 for (p in 1:dim(titers)[1]) {
 	if ( is.na(titers$Vx_2014[p])==FALSE & is.na(titers$Vx_2015[p])==FALSE ) {
 		if ( (titers$Vx_2014[p] == 'Inactivated') & (titers$Vx_2015[p] =='Inactivated') ) {
@@ -75,8 +68,6 @@ for (p in 1:dim(titers)[1]) {
 			titers$Vx_hx[p] <- 0
 		}
 	}
-	titers$H3_exp[p] <- subset(H3_exp,H3_exp$YOB==titers$YOB[p])$pH3
-	titers$T160K_exp[p] <- subset(T160K_exp,T160K_exp$YOB==titers$YOB[p])$p160K
 }
 
 #### Calculate people with any unknown status
@@ -108,29 +99,6 @@ FCV_col <- rgb(41,137,192,100,maxColorValue=255)
 FB_col <- rgb(18,153,20,100,maxColorValue=255)
 WT_col <- rgb(0,0,1,1/2)
 K_col <- rgb(1,0,0,1/2)
-
-############################################
-# (Figures) Pre- and post-vaccination trends 
-############################################
-
-pdf("Pre_post_vacc_by_YOB.pdf",width=6,height=6)
-par(mfrow=c(2,2), mar=c(4.1, 4, 0.9, 0.8) + 0.1)
-plot(jitter(titers$YOB),jitter(titers$GMT_Pre_WT,2),log="y",xlab="Birth year",ylab="Pre-vaccination WT GMT",xlim=c(1967,2000),ylim=c(7,10245),pch=16,col=WT_col,axes=FALSE)
-axis(1)
-axis(2, at = c(10,80,640,5120))
-
-plot(jitter(titers$YOB),jitter(titers$GMT_Pre_160K,2),log="y",xlab="Birth year",ylab="Pre-vaccination 160K GMT",xlim=c(1967,2000),ylim=c(7,10245),pch=16,col=K_col,axes=FALSE)
-axis(1)
-axis(2, at = c(10,80,640,5120))
-
-plot(jitter(titers$YOB),jitter(titers$GMT_Post_WT,2),log="y",xlab="Birth year",ylab="Post-vaccination WT GMT",xlim=c(1967,2000),ylim=c(7,10245),pch=16,col=WT_col,axes=FALSE)
-axis(1)
-axis(2, at = c(10,80,640,5120))
-
-plot(jitter(titers$YOB),jitter(titers$GMT_Post_160K,2),log="y",xlab="Birth year",ylab="Post-vaccination 160K GMT",xlim=c(1967,2000),ylim=c(7,10245),pch=16,col=K_col,axes=FALSE)
-axis(1)
-axis(2, at = c(10,80,640,5120))
-dev.off()
 
 
 ############################################
@@ -193,20 +161,37 @@ cor.test(titers$Age,titers$GMT_Post_160K,method='spearman')
 # Warning message:
 # In cor.test.default(titers$Age, titers$GMT_Post_160K, method = "spearman") :
   # Cannot compute exact p-value with ties
-  
+
+# 160K v. 160T, pre-vaccination
+wilcox.test(titers$GMT_Pre_160K,titers$GMT_Pre_WT,"greater")
+# Wilcoxon rank sum test with continuity correction
+#
+# data:  titers$GMT_Pre_160K and titers$GMT_Pre_WT
+# W = 3976.5, p-value = 7.703e-11
+# alternative hypothesis: true location shift is greater than 0
+
+# 160K v. 160T, post-vaccination
+wilcox.test(titers$GMT_Post_160K,titers$GMT_Post_WT,"greater")
+# Wilcoxon rank sum test with continuity correction
+#
+# data:  titers$GMT_Post_160K and titers$GMT_Post_WT
+# W = 3990, p-value = 6.458e-11
+# alternative hypothesis: true location shift is greater than 0
+
   
 ############################################
 ## (Figures) Fold change by vaccine group ##
 ############################################
 
-pdf("FC_group.pdf",width=6,height=3.2)
-#postscript("FC_group_easy_axes.ps")
+pdf("FC_group_combined.pdf",width=7.2,height=3.2)
 par(mfrow=c(1,2),mar=c(4, 4, 1, 1) + 0.1)
-plot(titers$Group,log(titers$GM_FC_WT,2),xlab="Vaccine type",ylab=expression("WT fold change"),col=alpha('blue',0.5),ylim=c(-1,7),axes=FALSE)
-axis(1,at=c(1,2,3),labels=c("FB","FCV","FZ"))
+plot(titers$Group,log(titers$GM_FC_WT,2),xlab="Vaccine type",ylab=expression("T160 HA fold change"),col=alpha('blue',0.5),ylim=c(-1,7),axes=FALSE,outpch=NA)
+points(jitter(as.numeric(titers$Group),factor=0.4),log(titers$GM_FC_WT,2),axes=FALSE,pch=16,col=alpha('blue',0.5))
+axis(1,at=c(1,2,3),labels=c("Flublok","Flucelvax","Fluzone"))
 axis(2,at=c(0,2,4,6),labels=c(1,4,16,64))
-plot(titers$Group,log(titers$GM_FC_160K,2),xlab="Vaccine type",ylab=expression("160K fold change"),col=alpha('red',0.5),ylim=c(-1,7),axes=FALSE)
-axis(1,at=c(1,2,3),labels=c("FB","FCV","FZ"))
+plot(titers$Group,log(titers$GM_FC_160K,2),xlab="Vaccine type",ylab=expression("K160 HA fold change"),col=alpha('red',0.5),ylim=c(-1,7),axes=FALSE, pch=16,outpch=NA)
+points(jitter(as.numeric(titers$Group),factor=0.4),log(titers$GM_FC_160K,2),axes=FALSE,pch=16,col=alpha('red',0.5))
+axis(1,at=c(1,2,3),labels=c("Flublok","Flucelvax","Fluzone"))
 axis(2,at=c(0,2,4,6),labels=c(1,4,16,64))
 dev.off()
 
@@ -214,28 +199,38 @@ dev.off()
 ############################################
 ## (Figures) Vaccination history and boost #
 ############################################
-pdf("Pre_FC_Vx_hx.pdf",width=6,height=6)
-par (mfrow=c(2,2))
+pdf("Pre_post_FC_Vx_hx.pdf",width=8,height=6)
+par (mfrow=c(2,3))
 
 # 160K pre-vacc titer by vaccination history
-plot(jitter(titers$Vx_hx),titers$GMT_Pre_160K,xlab="Vaccination history",ylab="Pre-vaccination 160K titer",log="y",ylim=c(7,10245),axes=FALSE,col=alpha('red',0.5),pch=16)
+plot(jitter(titers$Vx_hx),titers$GMT_Pre_160K,xlab="Vaccination history",ylab="Pre-vaccination titer",main="K160 HA",log="y",ylim=c(7,10245),axes=FALSE,col=alpha('red',0.5),pch=16)
+axis(1,at=c(0,1,2),labels=c("None","One","Both"))
+axis(2, at = c(10,80,640,5120))
+
+# 160K post-vacc titer by vaccination history
+plot(jitter(titers$Vx_hx),titers$GMT_Post_160K,xlab="Vaccination history",ylab="Post-vaccination titer",main="K160 HA",log="y",ylim=c(7,10245),axes=FALSE,col=alpha('red',0.5),pch=16)
 axis(1,at=c(0,1,2),labels=c("None","One","Both"))
 axis(2, at = c(10,80,640,5120))
 
 # 160K change by vaccination history
-plot(jitter(titers$Vx_hx),titers$GM_FC_160K,xlab="Vaccination history",ylab="160K fold change",ylim=c(0.4,81),axes=FALSE,log="y",col=alpha('red',0.5),pch=16)
+plot(jitter(titers$Vx_hx),titers$GM_FC_160K,xlab="Vaccination history",ylab="Fold change",main="K160 HA",ylim=c(0.4,81),axes=FALSE,log="y",col=alpha('red',0.5),pch=16)
 axis(1,at=c(0,1,2),labels=c("None","One","Both"))
-axis(2)
+axis(2,at=c(0.5,2.0,10.0,50.0))
 
 # WT pre-vacc by vaccination history
-plot(jitter(titers$Vx_hx),titers$GMT_Pre_WT,xlab="Vaccination history",ylab="Pre-vaccination WT titer",log="y",ylim=c(7,10245),axes=FALSE,col=alpha('blue',0.5),pch=16)
+plot(jitter(titers$Vx_hx),titers$GMT_Pre_WT,xlab="Vaccination history",ylab="Pre-vaccination titer",main="T160 HA", log="y",ylim=c(7,10245),axes=FALSE,col=alpha('blue',0.5),pch=16)
+axis(1,at=c(0,1,2),labels=c("None","One","Both"))
+axis(2, at = c(10,80,640,5120))
+
+# WT post-vacc by vaccination history
+plot(jitter(titers$Vx_hx),titers$GMT_Post_WT,xlab="Vaccination history",ylab="Post-vaccination titer",main="T160 HA",log="y",ylim=c(7,10245),axes=FALSE,col=alpha('blue',0.5),pch=16)
 axis(1,at=c(0,1,2),labels=c("None","One","Both"))
 axis(2, at = c(10,80,640,5120))
 
 # WT change by vaccination history
-plot(jitter(titers$Vx_hx),titers$GM_FC_WT,xlab="Vaccination history",ylab="WT fold change",log="y",ylim=c(0.4,81),axes=FALSE,col=alpha('blue',0.5),pch=16)
+plot(jitter(titers$Vx_hx),titers$GM_FC_WT,xlab="Vaccination history",ylab="Fold change",main="T160 HA", log="y",ylim=c(0.4,81),axes=FALSE,col=alpha('blue',0.5),pch=16)
 axis(1,at=c(0,1,2),labels=c("None","One","Both"))
-axis(2)
+axis(2,at=c(0.5,2.0,10.0,50.0))
 dev.off()  
 
 ############################################
@@ -245,7 +240,6 @@ dev.off()
 ############################################
 ## Prevacc titers to K160
 
-# Age effects
 fit <- lm( log(GMT_Pre_160K,2) ~ Age + factor(Vx_hx), data=titers)
 summary(fit); logLik(fit); extractAIC(fit)
 
@@ -274,39 +268,9 @@ summary(fit); logLik(fit); extractAIC(fit)
 # [1]  4.00000 71.52954
 
 
-# K160 effects
-fit <- lm( log(GMT_Pre_160K,2) ~ T160K_exp + factor(Vx_hx), data=titers)
-summary(fit); logLik(fit); extractAIC(fit)
-
-# Call:
-# lm(formula = log(GMT_Pre_160K, 2) ~ T160K_exp + factor(Vx_hx), 
-    # data = titers)
-
-# Residuals:
-    # Min      1Q  Median      3Q     Max 
-# -3.5892 -0.8808  0.0517  1.0969  3.3825 
-
-# Coefficients:
-               # Estimate Std. Error t value Pr(>|t|)    
-# (Intercept)      4.3926     0.8458   5.193 2.78e-06 ***
-# T160K_exp        1.9661     0.7085   2.775  0.00742 ** 
-# factor(Vx_hx)1   0.4940     0.7181   0.688  0.49430    
-# factor(Vx_hx)2   0.5838     0.6492   0.899  0.37227    
-# ---
-# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-
-# Residual standard error: 1.741 on 58 degrees of freedom
-  # (8 observations deleted due to missingness)
-# Multiple R-squared:  0.127,	Adjusted R-squared:  0.08179 
-# F-statistic: 2.811 on 3 and 58 DF,  p-value: 0.04725
-
-# 'log Lik.' -120.2914 (df=5)
-# [1]  4.00000 72.63443
-
 ############################################
 ## Prevacc titers to WT
 
-# Age effects
 fit <- lm( log(GMT_Pre_WT,2) ~ Age + factor(Vx_hx), data=titers)
 summary(fit); logLik(fit); extractAIC(fit)
 # Call:
@@ -334,39 +298,9 @@ summary(fit); logLik(fit); extractAIC(fit)
 # [1]  4.00000 26.45189
 
 
-# K160 effects
-fit <- lm( log(GMT_Pre_WT,2) ~ T160K_exp + factor(Vx_hx), data=titers)
-summary(fit); logLik(fit); extractAIC(fit)
-
-# Call:
-# lm(formula = log(GMT_Pre_WT, 2) ~ T160K_exp + factor(Vx_hx), 
-    # data = titers)
-
-# Residuals:
-     # Min       1Q   Median       3Q      Max 
-# -1.80732 -1.16085  0.00002  0.82933  2.71024 
-
-# Coefficients:
-               # Estimate Std. Error t value Pr(>|t|)    
-# (Intercept)      3.6870     0.5760   6.401 2.95e-08 ***
-# T160K_exp        0.8165     0.4826   1.692    0.096 .  
-# factor(Vx_hx)1   0.6258     0.4891   1.280    0.206    
-# factor(Vx_hx)2   0.1621     0.4422   0.367    0.715    
-# ---
-# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-
-# Residual standard error: 1.186 on 58 degrees of freedom
-  # (8 observations deleted due to missingness)
-# Multiple R-squared:  0.08695,	Adjusted R-squared:  0.03972 
-# F-statistic: 1.841 on 3 and 58 DF,  p-value: 0.1498
-
-# 'log Lik.' -96.4768 (df=5)
-# [1]  4.00000 25.00523
-
 ############################################
 ## Postvacc titers to K160
 
-# Age effects
 fit <- lm( log(GMT_Post_160K,2) ~ Age + factor(Vx_hx) + Group + log(GMT_Pre_160K,2), data=titers)
 summary(fit); logLik(fit); extractAIC(fit)
 
@@ -399,42 +333,9 @@ summary(fit); logLik(fit); extractAIC(fit)
 # [1]  7.00000 26.30267
 
 
-# K160 effects
-fit <- lm( log(GMT_Post_160K,2) ~ T160K_exp + factor(Vx_hx) + Group + log(GMT_Pre_160K,2), data=titers)
-summary(fit); logLik(fit); extractAIC(fit)
-
-# Call:
-# lm(formula = log(GMT_Post_160K, 2) ~ T160K_exp + factor(Vx_hx) + 
-    # Group + log(GMT_Pre_160K, 2), data = titers)
-
-# Residuals:
-     # Min       1Q   Median       3Q      Max 
-# -1.98130 -0.63666 -0.05465  0.59496  2.46341 
-
-# Coefficients:
-                     # Estimate Std. Error t value Pr(>|t|)    
-# (Intercept)           5.77175    0.72138   8.001 8.69e-11 ***
-# T160K_exp             1.18691    0.53950   2.200  0.03203 *  
-# factor(Vx_hx)1       -1.49163    0.48488  -3.076  0.00326 ** 
-# factor(Vx_hx)2       -1.92834    0.44808  -4.304 6.96e-05 ***
-# GroupFCV             -0.45796    0.36877  -1.242  0.21956    
-# GroupFZ               0.02737    0.38904   0.070  0.94416    
-# log(GMT_Pre_160K, 2)  0.45201    0.09423   4.797 1.27e-05 ***
-# ---
-# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-
-# Residual standard error: 1.168 on 55 degrees of freedom
-  # (8 observations deleted due to missingness)
-# Multiple R-squared:  0.5383,	Adjusted R-squared:  0.488 
-# F-statistic: 10.69 on 6 and 55 DF,  p-value: 7.607e-08
-
-# 'log Lik.' -93.91377 (df=8)
-# [1]  7.00000 25.87916
-
 ############################################
 ## Postvacc titers to WT
 
-# Age effects
 fit <- lm( log(GMT_Post_WT,2) ~ Age + factor(Vx_hx) + Group + log(GMT_Pre_WT,2), data=titers)
 summary(fit); logLik(fit); extractAIC(fit)
 
@@ -465,38 +366,3 @@ summary(fit); logLik(fit); extractAIC(fit)
 
 # 'log Lik.' -101.3966 (df=8)
 # [1]  7.00000 40.84475
-
-
-# K160 effects
-fit <- lm( log(GMT_Post_WT,2) ~ T160K_exp + factor(Vx_hx) + Group + log(GMT_Pre_WT,2), data=titers)
-summary(fit); logLik(fit); extractAIC(fit)
-
-# Call:
-# lm(formula = log(GMT_Post_WT, 2) ~ T160K_exp + factor(Vx_hx) + 
-    # Group + log(GMT_Pre_WT, 2), data = titers)
-
-# Residuals:
-    # Min      1Q  Median      3Q     Max 
-# -3.2706 -0.8431 -0.2298  0.7079  2.9355 
-
-# Coefficients:
-                   # Estimate Std. Error t value Pr(>|t|)    
-# (Intercept)          4.0998     0.8654   4.737 1.57e-05 ***
-# T160K_exp            0.4408     0.5739   0.768  0.44581    
-# factor(Vx_hx)1      -1.7134     0.5541  -3.092  0.00312 ** 
-# factor(Vx_hx)2      -2.2431     0.5065  -4.429 4.55e-05 ***
-# GroupFCV            -1.0723     0.4094  -2.619  0.01137 *  
-# GroupFZ             -0.8548     0.4378  -1.953  0.05596 .  
-# log(GMT_Pre_WT, 2)   0.8349     0.1502   5.559 8.21e-07 ***
-# ---
-# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-
-# Residual standard error: 1.322 on 55 degrees of freedom
-  # (8 observations deleted due to missingness)
-# Multiple R-squared:  0.5519,	Adjusted R-squared:  0.5031 
-# F-statistic: 11.29 on 6 and 55 DF,  p-value: 3.497e-08
-
-# 'log Lik.' -101.5898 (df=8)
-# [1]  7.00000 41.23129
-
-
